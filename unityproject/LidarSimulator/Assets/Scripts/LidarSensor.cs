0207cea53948d144c079069015458936b9dec15a
Philip Tibom <philiptibom@gmail.com>
2017-02-13 19:48:51 +0100

    Basic raycast & drawing debug line to display hits.

---
+++ unityproject/LidarSimulator/Assets/Scripts/LidarSensor.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LidarSensor : MonoBehaviour {
+    private Ray ray;
+    private RaycastHit hit;
+    private bool isHit;
+
+    // Use this for initialization
+    private void Start () {
+        Vector3 direction = transform.TransformDirection(Vector3.back);
+		ray = new Ray(transform.position, direction);
+    }
+	
+	// Update is called once per frame
+	private void Update () {
+        float distance = 100f;
+        if (isHit)
+        {
+            distance = hit.distance;
+        }
+        // For debugging, shows visible ray in real time.
+        Debug.DrawRay(ray.origin, ray.direction * distance, Color.red, 0.2f);
+    }
+
+    private void FixedUpdate()
+    {
+        ray.direction = transform.TransformDirection(Vector3.back);
+        ray.origin = transform.position;
+        isHit = Physics.Raycast(ray, out hit, 100.0f);
+        if (isHit)
+        {
+            print("Found object at distance: " + hit.distance);
+        }
+        else
+        {
+            print("No hit");
+        }
+    }
+}
