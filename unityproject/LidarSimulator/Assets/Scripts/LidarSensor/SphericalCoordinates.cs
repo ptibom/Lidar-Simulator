8251ef18e67b260c0ac142c0beb44e3e985b5bc5
tbbas <covernant_4@hotmail.com>
2017-02-24 15:08:20 +0100

    Wrote XML comments for classes

--- unityproject/LidarSimulator/Assets/Scripts/LidarSensor/SphericalCoordinates.cs
+++ unityproject/LidarSimulator/Assets/Scripts/LidarSensor/SphericalCoordinates.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using UnityEngine;
 
+/// <summary>
+/// A class representing spherical coordinates. These are created by the lidar sensor.
+/// </summary>
 public class SphericalCoordinates
 {
     private float radius;
@@ -15,6 +18,10 @@ public class SphericalCoordinates
     }
 
     // Constructor based on cartesian coordinates
+	/// <summary>
+	/// Initializes a new instance of the <see cref="SphericalCoordinates"/> class using cartesian coordinates.
+	/// </summary>
+	/// <param name="coordinates">Coordinates.</param>
     public SphericalCoordinates(Vector3 coordinates)
     {
         this.radius = Mathf.Sqrt(Mathf.Pow(coordinates.x, 2) + Mathf.Pow(coordinates.y, 2) + Mathf.Pow(coordinates.z, 2));
@@ -28,14 +35,26 @@ public class SphericalCoordinates
         this.azimuth = Mathf.Atan(coordinates.y / coordinates.x);
     }
 
+	/// <summary>
+	/// Gets the radius.
+	/// </summary>
+	/// <returns>The radius.</returns>
     private float GetRadius()
     {
         return this.radius;
     }
+	/// <summary>
+	/// Gets the inclination.
+	/// </summary>
+	/// <returns>The inclination.</returns>
     private float GetInclination()
     {
         return this.radius;
     }
+	/// <summary>
+	/// Gets the azimuth.
+	/// </summary>
+	/// <returns>The azimuth.</returns>
     private float GetAzimuth()
     {
         return this.azimuth;
