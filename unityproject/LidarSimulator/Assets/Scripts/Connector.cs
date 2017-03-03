b9db43effedd61f907211cac81b4486610a70b30
Tbbas <covernant_4@hotmail.com>
2017-03-03 15:12:23 +0100

    Connectro script for connecting sensor to cloud

---
+++ unityproject/LidarSimulator/Assets/Scripts/Connector.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Connector : MonoBehaviour {
+
+    public GameObject pointCloudBase;
+    public GameObject Lidar;
+
+    private PointCloud pointCloud;
+    private LidarSensor lidarSensor;
+
+
+	// Use this for initialization
+	void Start () {
+        pointCloud = pointCloudBase.GetComponent<PointCloud>();
+        lidarSensor = Lidar.GetComponent<LidarSensor>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        List<SphericalCoordinates> hits = lidarSensor.GetLastHits();
+        if (hits != null)
+        {
+            pointCloud.UpdatePoints((hits));
+
+        }
+        else
+        {
+            Debug.Log("Is null");
+        }
+    }
+}
