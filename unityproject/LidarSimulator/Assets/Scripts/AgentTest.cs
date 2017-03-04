d6c075820991b8f4d89a3b0d2cc3eff92efc25ba
KungKras <rickardl91@gmail.com>
2017-03-04 10:59:40 +0100

    Navmeshes, bilar som navigerar vägarna, etc

---
+++ unityproject/LidarSimulator/Assets/Scripts/AgentTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentTest : MonoBehaviour {
+	public Transform target;
+	public float minDist;
+
+	UnityEngine.AI.NavMeshAgent agent;
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<UnityEngine.AI.NavMeshAgent> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (target.GetComponent<WayPoint> () != null) {
+			Debug.Log ("Target is not null");
+			if (Vector3.Distance (transform.position, target.transform.position) < minDist) {
+				Debug.Log ("Reached Target, switching");
+				target = target.GetComponent<WayPoint> ().next.transform;
+			}
+		}
+		agent.SetDestination (target.position);
+	}
+}
---
+++ unityproject/LidarSimulator/Assets/Scripts/WayPoint.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WayPoint : MonoBehaviour {
+	public GameObject next;
+	public GameObject previous;
+
+	public static List<WayPoint> waypoints = new List<WayPoint>();
+	// Use this for initialization
+	void awake() {
+		waypoints.Add (this);
+	}
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
