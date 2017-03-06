d7f57a82f69ed0e80603de98976377acefe4acbb
Jonathan Jansson <jonathan.jansson0@gmail.com>
2017-03-06 22:56:47 +0100

    Correct aspect ratio of images in GUI. Finilized lidar menu

--- unityproject/LidarSimulator/Assets/Scripts/GUI/LidarMenuScript.cs
+++ unityproject/LidarSimulator/Assets/Scripts/GUI/LidarMenuScript.cs
@@ -11,89 +11,81 @@ public class LidarMenuScript : MonoBehaviour {
 	public Slider rotationSpeedHz;
 	public Slider rotationAnglePerStep;
 	public Slider rayDistance;
-	public Slider simulationSpeed;
 	public Slider upperFOV;
 	public Slider lowerFOV;
 	public Slider offset;
 	public Slider upperNormal;
 	public Slider lowerNormal;
+	public Slider simulationSpeed;
 
+	private float[] simSpeedValues = {0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.8f, 0.9f, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 		sensor = GameObject.FindGameObjectWithTag ("Lidar").GetComponent<LidarSensor> ();
-		UpdateGUIValues ();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+		InitializeGUIValues ();
 	}
 
-	void UpdateGUIValues(){
+	// A method which syncs the GUI lidar menu sliders with the initial values of the LidarSensor script
+	void InitializeGUIValues()
+	{
+		// Number of lasers
 		numberOfLasers.value = sensor.numberOfLasers;
 		SetSliderText (numberOfLasers);
+		// Rotationspeed
 		rotationSpeedHz.value = sensor.rotationSpeedHz;
 		SetSliderText(rotationSpeedHz);
+		// Rotationangle
 		rotationAnglePerStep.value = sensor.rotationAnglePerStep;
 		SetSliderText (rotationAnglePerStep);
-
-		//Skall fortsätta implementeras för alla parametrar
+		// Ray distance
+		rayDistance.value = sensor.rayDistance;
+		SetSliderText (rayDistance);
+		// Upper field of view
+		upperFOV.value = sensor.upperFOV;
+		SetSliderText (upperFOV);
+		// lower field of view
+		lowerFOV.value = sensor.lowerFOV;
+		SetSliderText (lowerFOV);
+		// Vertical offset between sets
+		offset.value = sensor.offset;
+		SetSliderText (offset);
+		// Normal of top set (Positive upwards from horizontal axis)
+		upperNormal.value = sensor.upperNormal;
+		SetSliderText (upperNormal);
+		// Normal of bottom set ((Positive downwards from horizontal axis)
+		lowerNormal.value = sensor.lowerNormal;
+		SetSliderText (lowerNormal);
+		// Simulationspeed
+		simulationSpeed.value = sensor.simulationSpeed;
+		SetSliderText (simulationSpeed);
 	}
 
-	public void SetSliderText(Slider slider){
+	// Sets the text on the handle of the passed slider to the value of the slider
+	public void SetSliderText(Slider slider)
+	{
 		slider.transform.FindChild ("Handle Slide Area").FindChild ("Handle").FindChild ("HandleText").GetComponent<Text> ().text = slider.value.ToString ();
 	}
 
-	public void RowsOfLasers(Toggle toggle){
-		if(toggle.isOn){
-		//	sensor.rowsOfLasers = 2;
-		} else {
-		//	sensor.rowsOfLasers = 1;
-		}
-	}
-	/*
-	public void RotationSpeed(Slider slider){
-		//sensor.rotationSpeedHz = slider.value;
-		slider.transform.FindChild ("Handle Slide Area").FindChild ("Handle").FindChild ("HandleText").GetComponent<Text> ().text = slider.value.ToString ();
-	}
-		
-	public void LasersPerSet(Slider slider){
-		sensor.rotationSpeedHz = slider.value;
-		slider.transform.FindChild ("Handle Slide Area").FindChild ("Handle").FindChild ("HandleText").GetComponent<Text> ().text = slider.value.ToString ();
+	// Sets the simulation speed in the LidarSensor script to the value in the GUI aswell as setting the handle text of the slider
+	public void UpdateSimulationSpeed()
+	{
+		simulationSpeed.transform.FindChild ("Handle Slide Area").FindChild ("Handle").FindChild ("HandleText").GetComponent<Text> ().text = simSpeedValues[ (int)simulationSpeed.value ].ToString ();
+		sensor.simulationSpeed = simSpeedValues[ (int)simulationSpeed.value ];
 	}
 
-	public void RotationAngle(Slider slider){
-		sensor.rotationAnglePerStep = slider.value;
-		slider.transform.FindChild ("Handle Slide Area").FindChild ("Handle").FindChild ("HandleText").GetComponent<Text> ().text = slider.value.ToString ();
+	// A method which syncs the LidarSensor script with the settings in the GUI
+	public void SendSettingsToLidar()
+	{
+		sensor.numberOfLasers = (int)numberOfLasers.value;
+		sensor.rotationSpeedHz = rotationSpeedHz.value;
+		sensor.rotationAnglePerStep = rotationAnglePerStep.value;
+		sensor.rayDistance = rayDistance.value;
+		sensor.upperFOV = upperFOV.value;
+		sensor.lowerFOV = lowerFOV.value;
+		sensor.offset = offset.value;
+		sensor.upperNormal = upperNormal.value;
+		sensor.lowerNormal = lowerNormal.value;
 	}
 
-	public void RayDistance(Slider slider){
-		sensor.rayDistance = slider.value;
-		slider.transform.FindChild ("Handle Slide Area").FindChild ("Handle").FindChild ("HandleText").GetComponent<Text> ().text = slider.value.ToString ();
-
-	}
-
-	public void TopVerticalFOV(Slider slider){
-		//sensor.verticalFOV = slider.value;
-		slider.transform.FindChild ("Handle Slide Area").FindChild ("Handle").FindChild ("HandleText").GetComponent<Text> ().text = slider.value.ToString ();
-	}
-
-	public void BottomVerticalFOV(Slider slider){
-		//sensor.verticalFOV = slider.value;
-		slider.transform.FindChild ("Handle Slide Area").FindChild ("Handle").FindChild ("HandleText").GetComponent<Text> ().text = slider.value.ToString ();
-	}
-
-	public void RowOffset(Slider slider){
-		sensor.offset = slider.value;
-		slider.transform.FindChild ("Handle Slide Area").FindChild ("Handle").FindChild ("HandleText").GetComponent<Text> ().text = slider.value.ToString ();
-
-	}
-
-	public void SimulationSpeed(Slider slider){
-		//sensor.simulationSpeed = slider.value;
-		slider.transform.FindChild ("Handle Slide Area").FindChild ("Handle").FindChild ("HandleText").GetComponent<Text> ().text = slider.value.ToString ();
-
-	}
-	*/
 }
