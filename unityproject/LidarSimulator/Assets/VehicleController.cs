f80e2826316a195b3f95760d9659a7cfafdc53d8
Swe ran <Swe ran>
2017-03-12 11:31:17 +0100

    vehicle without wheel collider

---
+++ unityproject/LidarSimulator/Assets/VehicleController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleController : MonoBehaviour {
+	public float speed=80;
+	float turnspeed=26;
+	float force = 200;
+	private float dir;
+	private Rigidbody rb;
+	// Use this for initialization
+
+	void Update(){
+		rb = GetComponent<Rigidbody> ();
+		force = Input.GetAxis ("Vertical");
+		dir = Input.GetAxis ("Horizontal");
+	}
+	void FixedUpdate(){
+		// Move forward and backward 
+		Vector3 movement = transform.forward * force * speed * Time.deltaTime;
+		rb.MovePosition (rb.position + movement);
+		// Steering
+		float turn = dir * turnspeed * Time.deltaTime;
+
+		// Rotation in the y axis and apply to the rigidbody's rotation.
+		Quaternion turnRotation = Quaternion.Euler (0f, turn, 0f);
+		rb.MoveRotation (rb.rotation * turnRotation);
+	}
+}
