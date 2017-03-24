2c96ac3aa3c63877421fbbda804065bb046fd2b3
Jonathan Jansson <jonathan.jansson0@gmail.com>
2017-03-24 16:32:20 +0100

    Implemented Home button and a camera swich for the menu buttons

--- unityproject/LidarSimulator/Assets/Scripts/GUI/LidarMenuScript.cs
+++ unityproject/LidarSimulator/Assets/Scripts/GUI/LidarMenuScript.cs
@@ -88,4 +88,5 @@ public class LidarMenuScript : MonoBehaviour {
 		sensor.lowerNormal = lowerNormal.value;
 	}
 
+
 }
--- unityproject/LidarSimulator/Assets/Scripts/GUI/MenuControllerScript.cs
+++ unityproject/LidarSimulator/Assets/Scripts/GUI/MenuControllerScript.cs
@@ -2,14 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEditor;
 
 public class MenuControllerScript : MonoBehaviour {
 
 	LidarSensor sensor;
-
+	GameObject mainCamera;
+	GameObject lidarCamera;
 	// Use this for initialization
 	void Start () {
 		sensor = GameObject.FindGameObjectWithTag ("Lidar").GetComponent<LidarSensor> ();
+		mainCamera = GameObject.FindGameObjectWithTag ("MainCamera");
+		lidarCamera = GameObject.Find ("LidarCamera");
+
+
+		//Set non used initial camera to inactive
+
+
 	}
 	
 	// Update is called once per frame
@@ -19,5 +28,13 @@ public class MenuControllerScript : MonoBehaviour {
 
 	public void PlayButton(){
 		//sensor.playSimulation = !sensor.playSimulation;
+	}
+		
+	public void SwitchCamera(GameObject cameraGObject){
+
+		mainCamera.SetActive(false);
+		lidarCamera.SetActive (false);
+
+		cameraGObject.SetActive (true);
 	}
 }
