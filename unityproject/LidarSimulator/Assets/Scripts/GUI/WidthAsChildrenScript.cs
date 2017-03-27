91cd2c500c0bcb47bff14ab11d3c5c5e18eaa2d3
Jonathan Jansson <jonathan.jansson0@gmail.com>
2017-03-27 16:09:04 +0200

    New, kind of disfunctional layout

---
+++ unityproject/LidarSimulator/Assets/Scripts/GUI/WidthAsChildrenScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class WidthAsChildrenScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        float maxWidth = 0;
+        for(int i =0; i< transform.childCount; i++)
+        {
+            Debug.Log("HEj" + transform.GetChild(i).GetComponent<RectTransform>().sizeDelta.y);
+            //if (childRectT.sizeDelta.x > maxWidth)
+            {
+              //  maxWidth = childRectT.sizeDelta.x;
+            }
+        }
+
+        gameObject.GetComponent<LayoutElement>().preferredWidth = maxWidth;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
