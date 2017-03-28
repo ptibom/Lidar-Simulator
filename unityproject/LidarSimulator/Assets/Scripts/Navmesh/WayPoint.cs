5135a5a5b49007061ff9e9246aa4cfad463fe4f6
KungKras <rickardl91@gmail.com>
2017-03-29 00:37:49 +0200

    Branchar ut från develop igen för att kunna fortsätta på skripten

--- unityproject/LidarSimulator/Assets/Scripts/Navmesh/WayPoint.cs
+++ unityproject/LidarSimulator/Assets/Scripts/Navmesh/WayPoint.cs
@@ -40,14 +40,53 @@ public class WayPoint : MonoBehaviour {
 	public void AddToPath(GameObject previousWayPoint) {
 		WayPoint previousWayPointScript = previousWayPoint.GetComponent<WayPoint>();
 		if (previousWayPointScript != null) {
-			previousWayPointScript.next = this.GameObject;
+			previousWayPointScript.next = this.gameObject;
 			previous = previousWayPointScript.GetGameObject();
 		}
 	
 	}
 
-	/*tar bort waypointen från path listan och fixar till next och previous referenserna och förstör gameobjectet*/
+	///*tar bort waypointen från path listan och fixar till next och previous referenserna och förstör gameobjectet*/
+	/*Om du har någon funktion som ska ta bort en waypoint så är det bäst att kalla på den här funktionen för den fixar så att vägen i övrigt är intakt*/
 	public void RemoveFromPath() {
+		/* för att det här ska funka får det aldrig finnas ett scenario när en next eller previous referens pekar på ett GameObject
+		 * utan WayPoint skriptet för då blir det pannkaka av allt*/
+
+
+		//WayPoint previousWayPointScript = previousWayPoint.GetComponent<WayPoint>();
+		//WayPoint nextWayPointScript = previousWayPoint.GetComponent<WayPoint>();
+		if(next != null && previous != null && next.GetComponent<WayPoint>().previousBranch != null){
+			
+		}
+		else if (next != null && previous != null && previousBranch != null) {
+
+			next.GetComponent<WayPoint> ().previous = previous;
+			previous.GetComponent<WayPoint> ().next = next;
+			previousBranch.GetComponent<WayPoint> ().next = previous;
+			previous.GetComponent<WayPoint> ().previousBranch = previousBranch;
+
+		}
+		else if(next == null && previous != null && previousBranch != null){
+			previousBranch.GetComponent<WayPoint> ().next = previous;
+			previous.GetComponent<WayPoint> ().previousBranch = previousBranch;
+			previous.GetComponent<WayPoint> ().next = null;
+
+
+		}
+		else if (next != null && previous != null) {
+			next.GetComponent<WayPoint> ().previous = previous;
+			previous.GetComponent<WayPoint> ().next = next;
+		} 
+		else if (next != null && previous == null) {
+			next.GetComponent<WayPoint> ().previous = null;
+		} 
+		else if (next == null && previous != null) {
+			
+		} 
+		else {
+		}
+
+		Destroy (gameObject);
 		/*int i = 0;
 		while (true) {
 			
@@ -64,16 +103,16 @@ public class WayPoint : MonoBehaviour {
 	
 	}*/
 
-	public static void removePath() {
-	
+	public static void removePath(GameObject wayPointInPath) {
+		//skapar en lista med alla waypoints i pathen och använder den för att förstöra hela pathen
 	
 	}
 
 	public void ClosePath(GameObject otherWayPoint, GameObject previousWayPoint) {
-	
+		next = 
 	}
 
 	public GameObject GetGameObject() {
-		return this.GameObject;
+		return this.gameObject;
 	}
 }
