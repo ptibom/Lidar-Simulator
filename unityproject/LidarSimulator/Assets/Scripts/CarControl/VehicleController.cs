cca73d052efff69060cd0f914cbc8272bdee7890
Swe ran <Swe ran>
2017-04-06 13:36:06 +0200

    moving the car with rigidbody

--- unityproject/LidarSimulator/Assets/Scripts/CarControl/VehicleController.cs
+++ unityproject/LidarSimulator/Assets/Scripts/CarControl/VehicleController.cs
@@ -4,35 +4,35 @@ using UnityEngine;
 
 public class VehicleController : MonoBehaviour {
 	public float speed=80;
-	float turnspeed=26;
-	float force = 200;
+	float turnspeed=20;
+	float force = 30;
 	private float dir;
 	private Rigidbody rb;
 
 	//Use this for initialization
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
 	void Update(){
-        //rb = GetComponent<Rigidbody>();
+        
         force = Input.GetAxis("Vertical");
         dir = Input.GetAxis("Horizontal");
     }
 	void FixedUpdate(){
 		// Move forward and backward 
 		Vector3 movement = transform.forward * force * speed * Time.deltaTime;
-        //rb.AddForce(transform.forward * force*speed);
-        //rb.MovePosition (rb.position + movement);
-        transform.Translate(movement);
+        rb.AddForce(transform.forward * force*speed);
+        rb.MovePosition (rb.position + movement);
+        //transform.Translate(movement);
         // Steering
         float turn = dir * turnspeed * Time.deltaTime;
 
 		// Rotation in the y axis and apply to the rigidbody's rotation.
 		Quaternion turnRotation = Quaternion.Euler (0f, turn, 0f);
-        //rb.MoveRotation (rb.rotation * turnRotation);
-        transform.rotation = transform.rotation * turnRotation;
+        rb.MoveRotation (rb.rotation * turnRotation);
+        //transform.rotation = transform.rotation * turnRotation;
 	}
 
 
