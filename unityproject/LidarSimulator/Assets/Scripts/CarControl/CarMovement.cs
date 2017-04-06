832021a3d4d68256c258627d23b5b3fa7f805c64
Swe ran <Swe ran>
2017-04-06 11:49:54 +0200

    adding CarMovement.cs which moves the car without using rigidbody

--- unityproject/LidarSimulator/Assets/Scripts/CarControl/CarMovement.cs
+++ unityproject/LidarSimulator/Assets/Scripts/CarControl/CarMovement.cs
@@ -4,26 +4,16 @@ using UnityEngine;
 
 public class CarMovement : MonoBehaviour {
 	// Update is called once per frame
-	public float setSpeed = 10f;
-
-	void Update () {
-		if(Input.GetKey(KeyCode.W)){
-			transform.Translate (Vector3.forward * setSpeed * Time.deltaTime);
-			transform.Rotate (0, 0, 0);		
-		}
-
-		if(Input.GetKey(KeyCode.A)){
-			transform.Translate (Vector3.left * setSpeed * Time.deltaTime);
-			transform.Rotate (0, -0.5f, 0);	
-		}
-		if(Input.GetKey(KeyCode.D)){
-			transform.Translate (Vector3.right* setSpeed * Time.deltaTime);
-			transform.Rotate (0, 0.5f, 0);	
-		}
-
-		if(Input.GetKey(KeyCode.S)){
-			transform.Translate (Vector3.back* setSpeed * Time.deltaTime);
-			transform.Rotate (0, 0, 0);
-		}
+	public float moveSpeed = 10f;
+    public float turnSpeed = 40f;
+    void Update () {
+		if(Input.GetKey(KeyCode.UpArrow))
+			transform.Translate (Vector3.forward * moveSpeed * Time.deltaTime);
+		if(Input.GetKey(KeyCode.DownArrow ))
+            transform.Translate (-Vector3.forward  * moveSpeed * Time.deltaTime);
+		if(Input.GetKey(KeyCode.LeftArrow ))
+			transform.Rotate  (Vector3.up , -turnSpeed * Time.deltaTime);
+		if(Input.GetKey(KeyCode.RightArrow ))
+			transform.Rotate  (Vector3.up ,turnSpeed * Time.deltaTime);
 	}
 }
