e96ebfa609645b146960f09a62926f5c481b2022
Jonathan Jansson <jonathan.jansson0@gmail.com>
2017-04-06 16:59:28 +0200

    Pause toggle prepared

---
+++ unityproject/LidarSimulator/Assets/PauseScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseScript : MonoBehaviour {
+
+	public GameObject timeController;
+
+	public void PauseToggle(){
+		if(gameObject.GetComponent<Toggle>().isOn){
+			
+		} else {
+			
+		}
+
+	}
+}
