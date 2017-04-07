bbef4bd0cd48213bf97fbffd00696c7207026048
Jonathan Jansson <jonathan.jansson0@gmail.com>
2017-04-07 20:43:23 +0200

    Lidar Mimic controllable through GUI

--- unityproject/LidarSimulator/Assets/Scripts/LidarSensor/LidarLineMimic.cs
+++ unityproject/LidarSimulator/Assets/Scripts/LidarSensor/LidarLineMimic.cs
@@ -5,11 +5,7 @@ using UnityEngine;
 public class LidarLineMimic : MonoBehaviour {
 	
 	public GameObject lineDrawerPrefab;
-
 	public int numberOfLasers = 2;
-	public float rotationSpeedHz = 1.0f;
-	public float rotationAnglePerStep = 45.0f;
-	public float rayDistance = 100f;
 	public float upperFOV = 30f;
 	public float lowerFOV = 30f;
 	public float offset = 0.001f;
@@ -17,7 +13,6 @@ public class LidarLineMimic : MonoBehaviour {
 	public float lowerNormal = 30f;
 
 	private int maxLasers = 64;
-
 	private List<LaserMimic> lasersMimics = new List<LaserMimic>();
 
 	// Use this for initialization
@@ -29,11 +24,9 @@ public class LidarLineMimic : MonoBehaviour {
 			lineDrawer.transform.position = transform.position;
 			lineDrawer.transform.rotation = transform.rotation;
 			lasersMimics.Add(new LaserMimic(0,0, lineDrawer, false));
-		
 		}
 	}
-
-
+		
 	public void UpdateLines(){
 		float upperTotalAngle = upperFOV / 2;
 		float lowerTotalAngle = lowerFOV / 2;
@@ -44,13 +37,13 @@ public class LidarLineMimic : MonoBehaviour {
 		{
 			if (i < numberOfLasers/2)
 			{
-				lasersMimics[i].SetRayParameters(lowerTotalAngle + lowerNormal, -offset);
+				lasersMimics[i].SetRayParameters(lowerTotalAngle + lowerNormal, -offset, transform);
 				lasersMimics [i].SetActive (true);
 				lowerTotalAngle -= lowerAngle;
 			}
 			else
 			{
-				lasersMimics[i].SetRayParameters(upperTotalAngle - upperNormal, 0);
+				lasersMimics[i].SetRayParameters(upperTotalAngle - upperNormal, 0, transform);
 				lasersMimics [i].SetActive (true);
 				upperTotalAngle -= upperAngle;
 			}
@@ -72,7 +65,7 @@ public class LidarLineMimic : MonoBehaviour {
 
 	public void DestroyLaserMimics(){
 		foreach(LaserMimic lm in lasersMimics){
-			Destroy (lm.lineDrawer);
+			Destroy (lm.GetLineDrawerGObject());
 		}
 		lasersMimics.Clear ();
 	}
@@ -82,52 +75,25 @@ public class LidarLineMimic : MonoBehaviour {
 		private float offset;
 		private RenderLine lineRenderer;
 		private bool rayOn;
-		private Transform originalTransform;
 		private float rayDistance = 5f;
-
-		public GameObject lineDrawer;
+		private GameObject lineDrawer;
 
 		public LaserMimic(float verticalAngle, float offset, GameObject lineDrawer, bool rayOn){
 			this.verticalAngle = verticalAngle;
 			this.offset = offset;
 			this.lineDrawer = lineDrawer;
 			lineRenderer = lineDrawer.GetComponent<RenderLine>();
-
-			originalTransform = lineDrawer.transform;
-
-			lineDrawer.transform.position = originalTransform.position + (originalTransform.up * offset);
+			lineDrawer.transform.position = lineDrawer.transform.position + (lineDrawer.transform.up * offset);
 
 			this.rayOn = rayOn;
-		
-		
-			/*
-			Quaternion q = Quaternion.AngleAxis(verticalAngle, Vector3.right);
-			Vector3 direction = parentObject.transform.TransformDirection(q * Vector3.forward);
-			ray.origin = parentObject.transform.position + (parentObject.transform.up * offset);
-			ray.direction = direction;
-			*/
 		}
 
-		public void SetRayParameters(float verticalAngle, float offset){
+		public void SetRayParameters(float verticalAngle, float offset, Transform baseTransform){
 			this.verticalAngle = verticalAngle;
 			this.offset = offset;
-			lineDrawer.transform.position = originalTransform.position + (originalTransform.up * offset);
-
-			//Debug.Log (new Vector3 (0, verticalAngle, 0));
-			lineDrawer.transform.rotation = originalTransform.rotation;
+			lineDrawer.transform.position = baseTransform.position + (baseTransform.up * offset);
+			lineDrawer.transform.rotation = baseTransform.rotation;
 			lineDrawer.transform.Rotate (new Vector3 (verticalAngle, 0, 0));
-
-			//lineDrawer.transform.rotation = Quaternion.Euler(0,verticalAngle,0);
-			//lineDrawer.transform.eulerAngles = Quaternion.Euler(0, verticalAngle, 0).eulerAngles;
-			//Debug.Log (lineDrawer.transform.rotation.eulerAngles);
-
-			//Quaternion q = Quaternion.AngleAxis(verticalAngle, Vector3.right);
-			//Vector3 dir = lineDrawer.transform.TransformDirection(q * Vector3.forward);
-			//Debug.Log (dir);
-			//lineDrawer.transform.rotation = Quaternion.Euler(dir.x, dir.y, dir.z);
-
-
-
 		}
 
 		public void SetActive(bool state){
@@ -138,8 +104,14 @@ public class LidarLineMimic : MonoBehaviour {
 			if (rayOn) {
 				lineRenderer.DrawLine (lineDrawer.transform.forward * rayDistance + lineRenderer.transform.position);
 			} else {
-				lineRenderer.DrawLine (originalTransform.position);
+				lineRenderer.DrawLine (lineDrawer.transform.position);
 			}
 		}
+
+
+		public GameObject GetLineDrawerGObject()
+		{
+			return lineDrawer;
+		}	
 	}
 }
