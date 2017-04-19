09bb15dd5a3c4e8740cd1e73f207ab75328f08db
Jonathan Jansson <jonathan.jansson0@gmail.com>
2017-04-19 19:15:27 +0200

    Reimplemented new materials in scene

---
+++ unityproject/LidarSimulator/Assets/Scripts/TransparentMateralFix.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Fixes unitys rendering order problem with transparent materials
+/// 
+/// @author: Jonathan Jansson
+/// </summary>
+public class TransparentMaterialFix : MonoBehaviour {
+
+    public Material mat1;
+    public Material mat2;
+    public Material mat3;
+    
+    void Start () {
+		mat1.SetInt("_ZWrite", 1);
+        mat2.SetInt("_ZWrite", 1);
+        mat3.SetInt("_ZWrite", 1);
+    }
+}
