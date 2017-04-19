f274e02a62ca1d5251f895f2f9240e3beb8c9dde
Tobias Alldén <allden@net.chalmers.se>
2017-04-19 20:49:41 +0200

    Short script for main menu button

---
+++ unityproject/LidarSimulator/Assets/Scripts/GUI/MainMenuButtonScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtonScript : MonoBehaviour {
+    private Button toMainMenu;
+
+	// Use this for initialization
+	void Start () {
+        toMainMenu = GameObject.Find("MainMenuButton").GetComponent<Button>();
+        toMainMenu.onClick.AddListener(LoadMain);
+
+
+    }
+
+    // Update is called once per frame
+    void LoadMain () {
+        SceneManager.LoadSceneAsync(0, LoadSceneMode.Single);
+    }
+}
