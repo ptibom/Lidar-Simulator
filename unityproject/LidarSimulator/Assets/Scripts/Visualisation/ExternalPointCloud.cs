3a48f9511bf2d59a739c6dd137842ee36236d694
Tobias Alldén <allden@net.chalmers.se>
2017-04-24 09:36:20 +0200

    MeshBase prefab, wrote splitinto chunks and instantiate methods in script

--- unityproject/LidarSimulator/Assets/Scripts/Visualisation/ExternalPointCloud.cs
+++ unityproject/LidarSimulator/Assets/Scripts/Visualisation/ExternalPointCloud.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class ExternalPointCloud : MonoBehaviour {
+    public GameObject meshObject;
+
+
     private List<Mesh> meshes;
-    int numPoints = 60000;
+    private int maxParticlesPerChunk = 65000;
+    
 
     // Use this for initialization
     void Start()
@@ -15,9 +18,15 @@ public class ExternalPointCloud : MonoBehaviour {
         GetComponent<MeshFilter>().mesh = startMesh;
     }
 
-    void CreateMesh(Vector3[] worldPoints, int[] indices, Color[] colors)
+    void CreateMesh(LinkedList<SphericalCoordinate> coordinates)
     {
-        if(worldPoints.Length > 65000)
+
+        if (coordinates.Count > maxParticlesPerChunk) // Max particle size per mesh 
+        {
+            List<LinkedList<SphericalCoordinate>> chunks = SplitIntoChunks(coordinates);
+        }
+        /**
+        if (worldPoints.Length > maxParticlesPerChunk)
         {
             //TODO: 
         } else
@@ -27,8 +36,53 @@ public class ExternalPointCloud : MonoBehaviour {
             meshes[0].SetIndices(indices,MeshTopology.Points,0);
             
         }      
+    **/
 
     }
+
+    /// <summary>
+    /// Splits a list of spherical coordinates into chunks of length 64000, the max number of particles per mesh.
+    /// </summary>
+    /// <param name="coordinates">The coordinates to be split</param>
+    /// <returns>A list containing the chunks of points</returns>
+    private List<LinkedList<SphericalCoordinate>> SplitIntoChunks(LinkedList<SphericalCoordinate> coordinates)
+    {
+        List<LinkedList<SphericalCoordinate>> chunks = new List<LinkedList<SphericalCoordinate>>();
+        LinkedList<SphericalCoordinate> currentChunk = new LinkedList<SphericalCoordinate>();
+
+        for(LinkedListNode<SphericalCoordinate> it = coordinates.First; it != null; it = it.Next)
+        {
+            currentChunk.AddLast(it.Value);
+            if(currentChunk.Count%maxParticlesPerChunk == 0)
+            {
+                chunks.Add(currentChunk);
+                currentChunk = new LinkedList<SphericalCoordinate>();
+            } else if(it.Next == null)
+            {
+                chunks.Add(currentChunk); // final value
+            }
+        }
+        return chunks;
+    }
+
+
+    /// <summary>
+    /// Creates the given number of meshes(the meshprefab)
+    /// </summary>
+    /// <param name="count"></param>
+    /// <returns>A list containing the names of the created prefabs</returns>
+    private List<string> CreateNeededMeshes(int count)
+    {
+        List<string> nameList = new List<string>();
+        for(int i = 0; i< count; i++)
+        {
+            GameObject newGO = Instantiate(meshObject, new Vector3(0,0,0), Quaternion.identity);
+            newGO.name = "MeshObject" + i;
+        }
+
+        return nameList;
+    }
+
 
 }
 
