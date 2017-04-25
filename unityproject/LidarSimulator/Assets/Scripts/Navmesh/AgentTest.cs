4579e955f11f548d492b08240b21655438c06259
KungKras <rickardl91@gmail.com>
2017-04-25 17:13:00 +0200

    WayPoint och Agenter lyssnar nu på playknappen

--- unityproject/LidarSimulator/Assets/Scripts/Navmesh/AgentTest.cs
+++ unityproject/LidarSimulator/Assets/Scripts/Navmesh/AgentTest.cs
@@ -9,11 +9,42 @@ public class AgentTest : MonoBehaviour {
 
 	UnityEngine.AI.NavMeshAgent agent;
 
-	// Use this for initialization
-	void Start () {
-		agent = GetComponent<UnityEngine.AI.NavMeshAgent> ();
+	public Animator anime;
+
+	void Awake() {
+		PlayButton.OnPlayToggled += StartOrFreeze;
 	}
-	
+
+	public void StartOrFreeze(bool b){
+		if (b == true) {
+			Resume ();
+		} 
+		else if (b == false) {
+			Freeze ();
+		}
+	}
+
+	// Use this for initialization
+	void Start() {
+		agent = GetComponent<UnityEngine.AI.NavMeshAgent> ();
+		anime = GetComponent<Animator> ();
+		Freeze ();
+	}
+
+	public void Freeze() {
+		agent.SetDestination (transform.position);
+		if (anime != null) {
+			anime.enabled = false;
+		}
+	}
+
+	public void Resume() {
+		agent.SetDestination (target);
+		if (anime != null) {
+			anime.enabled = true;
+		}
+	}
+
 	// Update is called once per frame
 	void Update () {
 		if (target != null) {
