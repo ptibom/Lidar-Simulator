bdedaaa09e892c7746a2e1ac552edac2de52b84c
Jonathan Jansson <jonathan.jansson0@gmail.com>
2017-04-26 13:32:34 +0200

    New font colors in editor. "Fixed" more transparency bugs

--- unityproject/LidarSimulator/Assets/Scripts/TransperentMaterialFix.cs
+++ unityproject/LidarSimulator/Assets/Scripts/TransperentMaterialFix.cs
@@ -11,11 +11,15 @@ public class TransperentMaterialFix : MonoBehaviour {
     public Material mat1;
     public Material mat2;
     public Material mat3;
+    public Material mat4;
+    public Material mat5;
 
     void Start()
     {
         mat1.SetInt("_ZWrite", 1);
         mat2.SetInt("_ZWrite", 1);
         mat3.SetInt("_ZWrite", 1);
+        mat4.SetInt("_ZWrite", 1);
+        mat5.SetInt("_ZWrite", 1);
     }
 }
